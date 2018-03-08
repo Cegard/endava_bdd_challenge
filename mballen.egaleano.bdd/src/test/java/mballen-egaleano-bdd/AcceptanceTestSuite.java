d5ac0ed623cc85bcfb428232548db96eeaa80d38
Miguel Angel Ballen <mballeng91@EN911111.endava.net>
2018-03-08 11:28:39 -0500

    Chore : Project refactor

--- mballen.egaleano.bdd/src/test/java/mballen-egaleano-bdd/AcceptanceTestSuite.java
+++
@@ -1,5 +0,0 @@
-package mballen-egaleano-bdd;
-
-import net.serenitybdd.jbehave.SerenityStories;
-
-public class AcceptanceTestSuite extends SerenityStories {}
---
+++ mballen.egaleano.bdd/src/test/java/mballenegaleanobdd/AcceptanceTestSuite.java
@@ -0,0 +1,5 @@
+package mballenegaleanobdd;
+
+import net.serenitybdd.jbehave.SerenityStories;
+
+public class AcceptanceTestSuite extends SerenityStories {}
